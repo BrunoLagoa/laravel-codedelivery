a639b44a3a5f24a9ed799559860d9a3fd0818d25
BrunoLagoa <miroldols@gmail.com>
2015-11-13 01:04:20 -0200

    - LaravelCordova-2.5-RepositoryServiceProvider  Changes to be committed: 	new file:   app/Providers/RepositoryServiceProvider.php 	modified:   config/app.php

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodeDelivery\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'CodeDelivery\Repositories\CategoryRepository',
+            'CodeDelivery\Repositories\CategoryRepositoryEloquent'
+        );
+    }
+}
--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         CodeDelivery\Providers\AuthServiceProvider::class,
         CodeDelivery\Providers\EventServiceProvider::class,
         CodeDelivery\Providers\RouteServiceProvider::class,
+        CodeDelivery\Providers\RepositoryServiceProvider::class,
 
     ],
 

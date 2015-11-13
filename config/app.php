c3cd1c2a3df235fada4cf13f7bd491a933f67383
BrunoLagoa <miroldols@gmail.com>
2015-11-13 01:11:23 -0200

    - LaravelCordova-3.1-Instalando Views Basicas  Changes to be committed: 	modified:   composer.json 	modified:   composer.lock 	modified:   config/app.php 	new file:   resources/views/app.blade.php 	new file:   resources/views/auth/login.blade.php 	new file:   resources/views/auth/password.blade.php 	new file:   resources/views/auth/register.blade.php 	new file:   resources/views/auth/reset.blade.php 	new file:   resources/views/emails/password.blade.php 	new file:   resources/views/home.blade.php

--- config/app.php
+++ config/app.php
@@ -138,6 +138,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        Bestmomo\Scafold\ScafoldServiceProvider::class,
 
         /*
          * Application Service Providers...

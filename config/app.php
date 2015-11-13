31a50557b980915e692c9475a24ff83788b37e22
BrunoLagoa <miroldols@gmail.com>
2015-11-12 23:53:38 -0200

    - LaravelCordova-2.1-Introducao Repositories  - LaravelCordova-2.2-Instalando Package para Repositories  Changes to be committed: 	modified:   composer.json 	modified:   composer.lock 	modified:   config/app.php 	new file:   config/repository.php

--- config/app.php
+++ config/app.php
@@ -137,6 +137,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+
         /*
          * Application Service Providers...
          */

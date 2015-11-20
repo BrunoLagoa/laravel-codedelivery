ead9c81c812d0b7020d4b96a9caf8bf550bde682
BrunoLagoa <miroldols@gmail.com>
2015-11-20 20:17:24 -0200

    - LaravelCordova-3.21-Validando Role  Changes to be committed: 	modified:   app/Http/Middleware/CheckRole.php 	modified:   database/seeds/UserTableSeeder.php

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -19,6 +19,11 @@ class CheckRole
         if(!Auth::check()){
            return redirect('/auth/login');
         }
+
+        if(Auth::user()->role <> "admin"){
+            return redirect('/auth/login');
+        }
+
         return $next($request);
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,6 +14,21 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         //factory(User::class, 10)->create();
+        factory(User::class)->create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt(123456),
+            'role' => 'admin',
+            'remember_token' => str_random(10),
+        ]);
+
         factory(User::class, 10)->create()->each(function($u) {
            $u->client()->save(factory(Client::class)->make());
         });

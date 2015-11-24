4bf1c35b1656f91e751ac82a5da5b91148b217e9
BrunoLagoa <miroldols@gmail.com>
2015-11-24 00:07:41 -0200

    - LaravelCordova-4.3-Editando client  Changes to be committed: 	modified:   resources/views/admin/clients/_form.blade.php 	modified:   resources/views/admin/clients/edit.blade.php

--- resources/views/admin/clients/_form.blade.php
+++ resources/views/admin/clients/_form.blade.php
@@ -1,5 +1,41 @@
 <!-- Form Input -->
 <div class="form-group">
     {!! Form::label('Name', 'Nome:') !!}
-    {!! Form::text('name',null, ['class'=>'form-control']) !!}
+    {!! Form::text('user[name]',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Email', 'Email:') !!}
+    {!! Form::text('user[email]',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Telefone', 'Telefone:') !!}
+    {!! Form::text('phone',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Endereço', 'Endereço:') !!}
+    {!! Form::textarea('address',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Cidade', 'Cidade:') !!}
+    {!! Form::text('city',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Estado', 'Estado:') !!}
+    {!! Form::text('state',null, ['class'=>'form-control']) !!}
+</div>
+
+<!-- Form Input -->
+<div class="form-group">
+    {!! Form::label('Cep', 'Cep:') !!}
+    {!! Form::text('zipcode',null, ['class'=>'form-control']) !!}
 </div>
--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 
 <div class="container">
-    <h3>Editando Categoria: {{ $category->name }}</h3>
+    <h3>Editando Cliente: {{ $client->user->name }}</h3>
 
     <!-- Errors -->
     @include('errors._check')
 
-    {!! Form::model($category,['route'=>['admin.categories.update', $category->id]]) !!}
+    {!! Form::model($client,['route'=>['admin.clients.update', $client->id]]) !!}
 
     <!-- Form Input -->
-    @include('admin.categories._form')
+    @include('admin.clients._form')
 
     <div class="form-group">
-        {!! Form::submit('Salvar Categoria',['class'=>'btn btn-primary']) !!}
+        {!! Form::submit('Salvar',['class'=>'btn btn-primary']) !!}
     </div>
 
     {!! Form::close() !!}

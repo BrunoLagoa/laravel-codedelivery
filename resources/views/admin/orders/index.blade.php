70c41ab99a32c774ddfae969d5bd9508b8d3bcb8
BrunoLagoa <miroldols@gmail.com>
2015-11-24 02:24:50 -0200

    - LaravelCordova-5.3-Listando Orders  Changes to be committed: 	modified:   resources/views/admin/orders/index.blade.php

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -9,13 +9,35 @@
             <thead>
             <tr>
                 <th>ID</th>
+                <th>Total</th>
+                <th>Data</th>
+                <th>Items</th>
+                <th>Entregador</th>
+                <th>Status</th>
                 <th>Ação</th>
             </tr>
             </thead>
             <tbody>
             @foreach($orders as $order)
                 <tr>
-                    <td>{{ $order->id }}</td>
+                    <td>#{{ $order->id }}</td>
+                    <td>R$ {{ $order->total }}</td>
+                    <td>{{ $order->created_at }}</td>
+                    <td>
+                        <ul>
+                            @foreach($order->items as $item)
+                            <li>{{ $item->product->name }}</li>
+                            @endforeach
+                        </ul>
+                    </td>
+                    <td>
+                        @if($order->deliveryman)
+                            {{$order->deliveryman->name}}
+                        @else
+                            ---
+                        @endif
+                    </td>
+                    <td>{{ $order->status }}</td>
                     <td><a href="" class="btn btn-default btn-sm">Editar</a></td>
                 </tr>
             @endforeach

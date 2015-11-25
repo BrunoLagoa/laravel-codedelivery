eb62258d365b31c383c86c7d7b0dbdd9cf8c399a
BrunoLagoa <miroldols@gmail.com>
2015-11-25 02:14:40 -0200

    - LaravelCordova-6.7-Criando form de pedido  Changes to be committed: 	modified:   resources/views/app.blade.php 	modified:   resources/views/customer/order/create.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -67,5 +67,7 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	@yield('post-script')
 </body>
 </html>
--- resources/views/customer/order/create.blade.php
+++ resources/views/customer/order/create.blade.php
@@ -12,7 +12,7 @@
             <div class="form-group">
                 <label>Total:</label>
                 <p id="total"></p>
-                <a href="#" class="btn btn-success">Novo Item</a>
+                <a href="#" id="btnNewItem" class="btn btn-success">Novo Item</a>
                 <br /> <br />
 
                 <table class="table table-bordered">
@@ -43,4 +43,25 @@
         </div>
 
     </div>
+@endsection
+
+@section('post-script')
+    <script>
+        $('#btnNewItem').click(function(){
+           var row = $('table tbody > tr:last'),
+                   newRow = row.clone(),
+                   length = $("table tbody tr").length;
+
+            newRow.find('td').each(function(){
+                var td = $(this),
+                        input = td.find('input,select'),
+                        name = input.attr('name');
+
+                input.attr('name', name.replace((length - 1) + "", length + ""));
+            });
+
+            newRow.find('input').val(1);
+            newRow.insertAfter(row);
+        });
+    </script>
 @endsection

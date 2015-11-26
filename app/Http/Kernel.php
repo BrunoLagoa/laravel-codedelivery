75a7d89ea551b5973b265fe769d3d6fc4a2c83d0
BrunoLagoa <miroldols@gmail.com>
2015-11-26 01:52:47 -0200

    - LaravelCordova-8.1-Falando do OAuth2  - LaravelCordova-8.2-Instalando oauth2-server-laravel  Changes to be committed: 	modified:   app/Http/Kernel.php 	modified:   composer.json 	modified:   composer.lock 	modified:   config/app.php 	new file:   config/oauth2.php 	new file:   database/migrations/2014_04_24_110151_create_oauth_scopes_table.php 	new file:   database/migrations/2014_04_24_110304_create_oauth_grants_table.php 	new file:   database/migrations/2014_04_24_110403_create_oauth_grant_scopes_table.php 	new file:   database/migrations/2014_04_24_110459_create_oauth_clients_table.php 	new file:   database/migrations/2014_04_24_110557_create_oauth_client_endpoints_table.php 	new file:   database/migrations/2014_04_24_110705_create_oauth_client_scopes_table.php 	new file:   database/migrations/2014_04_24_110817_create_oauth_client_grants_table.php 	new file:   database/migrations/2014_04_24_111002_create_oauth_sessions_table.php 	new file:   database/migrations/2014_04_24_111109_create_oauth_session_scopes_table.php 	new file:   database/migrations/2014_04_24_111254_create_oauth_auth_codes_table.php 	new file:   database/migrations/2014_04_24_111403_create_oauth_auth_code_scopes_table.php 	new file:   database/migrations/2014_04_24_111518_create_oauth_access_tokens_table.php 	new file:   database/migrations/2014_04_24_111657_create_oauth_access_token_scopes_table.php 	new file:   database/migrations/2014_04_24_111810_create_oauth_refresh_tokens_table.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \CodeDelivery\Http\Middleware\VerifyCsrfToken::class,
+        \LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware::class
     ];
 
     /**
@@ -30,5 +31,9 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \CodeDelivery\Http\Middleware\RedirectIfAuthenticated::class,
         'auth.checkrole' => \CodeDelivery\Http\Middleware\CheckRole::class,
+        'oauth' => \LucaDegasperi\OAuth2Server\Middleware\OAuthMiddleware::class,
+        'oauth-user' => \LucaDegasperi\OAuth2Server\Middleware\OAuthUserOwnerMiddleware::class,
+        'oauth-client' => \LucaDegasperi\OAuth2Server\Middleware\OAuthClientOwnerMiddleware::class,
+        'check-authorization-params' => \LucaDegasperi\OAuth2Server\Middleware\CheckAuthCodeRequestMiddleware::class
     ];
 }
--- config/app.php
+++ config/app.php
@@ -141,6 +141,9 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         Bestmomo\Scafold\ScafoldServiceProvider::class,
 
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -200,6 +203,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Html'      =>Illuminate\Html\HtmlFacade::class,
         'Form'      =>Illuminate\Html\FormFacade::class,
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
 
     ],
 

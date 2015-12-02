223071592db2a058eac57e3a6a77d3c8276173bf
BrunoLagoa <miroldols@gmail.com>
2015-12-02 00:33:06 -0200

    - LaravelCordova-10.3-Validando order de client - parte 2 Changes to be committed: 	modified:   app/Http/Requests/CheckoutRequest.php

--- app/Http/Requests/CheckoutRequest.php
+++ app/Http/Requests/CheckoutRequest.php
@@ -2,6 +2,8 @@
 
 namespace CodeDelivery\Http\Requests;
 
+use Illuminate\Http\Request as HttpRequest;
+
 class CheckoutRequest extends Request
 {
     /**
@@ -15,14 +17,26 @@ class CheckoutRequest extends Request
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
+     * @param HttpRequest $request
      * @return array
      */
-    public function rules()
+    public function rules(HttpRequest $request)
     {
-        return [
-            'cupom_code' => 'exists:cupoms,code,used,0'
+        $rules = [
+            'cupom_code' => 'exists:cupoms,code,used,0',
         ];
+        $this->buildRulesItems(0, $rules);
+        $items = $request->get('items', []);
+        $items = !is_array($items) ? [] : $items;
+        foreach ($items as $key => $val) {
+            $this->buildRulesItems($key, $rules);
+        }
+        return $rules;
+    }
+
+    public function buildRulesItems($key, array &$rules)
+    {
+        $rules["items.$key.product_id"] = 'required';
+        $rules["items.$key.qtd"] = 'required';
     }
 }

3371ab775a21f1a9b991bbfadf5cd751a8103d4d
BrunoLagoa <miroldols@gmail.com>
2015-12-04 12:06:54 -0200

    - LaravelIonicCordova-12.6-Trabalhando com $state e $stateParams Changes to be committed: 	modified:   ionic/www/index.html 	modified:   ionic/www/js/app.js 	new file:   ionic/www/js/controllers/home.js 	modified:   ionic/www/templates/home.html

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','starter.controllers'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -20,8 +20,9 @@ angular.module('starter', ['ionic'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('home',{
-                url: '/home',
-                templateUrl: 'templates/home.html'
+                url: '/home/:nome',
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
             })
             .state('home.a',{
                 url: '/a',

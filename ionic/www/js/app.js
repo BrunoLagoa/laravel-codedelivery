091a723196f962c15ffe7b49b2b79bef047445f8
BrunoLagoa <miroldols@gmail.com>
2015-12-04 10:33:45 -0200

    - LaravelIonicCordova-11.14-ion-lab  - LaravelIonicCordova-11.15-ion emulate  - LaravelIonicCordova-12.1-Introducao ao ui-router  - LaravelIonicCordova-12.2-Criando primeira navegacao Changes to be committed: 	modified:   ionic/www/index.html 	modified:   ionic/www/js/app.js 	new file:   ionic/www/templates/home.html 	new file:   ionic/www/templates/main.html

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -5,15 +5,25 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function($stateProvider) {
+        $stateProvider.state('home',{
+            url: '/home',
+            templateUrl: 'templates/home.html'
+        })
+            .state('main',{
+                url: '/',
+                templateUrl: 'templates/main.html'
+            })
+    });
